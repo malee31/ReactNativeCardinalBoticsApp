2605ac3b232ac8772fc22c5425fef0ff826b337c
Marvin Lee <43216761+malee31@users.noreply.github.com>
2020-08-21 19:58:41 -0700

    Added a Navigation Element. Also, doesn't crash now!

--- App.js
+++ App.js
@@ -1,37 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
-import { StyleSheet, Text, View, TouchableHighlight, Navigator } from 'react-native';
+import Home from "./Home";
+import NotHome from "./NotHome";
 
-const routes = [
-	{title: "Home", index: 0},
-	{title: "Not Home", index: 1}
-];
+const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <Navigator
-		initialRoute={routes[0]}
-		initialRouteStack={routes}
-		renderScene={(route, navigator) =>
-			<TouchableHighlight onPress={() => {
-				if(route.index == 0) {
-					navigator.push(routes[1]);
-				} else {
-					navigator.pop();
-				}
-			}}>
-			<Text> Hello {route.title}! </Text>
-			</TouchableHighlight>
-			}
-		style={styles.container}/>
-  );
+	return (
+		<NavigationContainer style={styles.container}>
+			<Stack.Navigator>
+				<Stack.Screen
+					name="Home"
+					component={Home}
+					options={{ title: 'Welcome' }}
+				/>
+				<Stack.Screen
+					name="NotHome"
+					component={NotHome}
+					options={{ title: 'Welcome II' }}
+				/>
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
 }
 
+
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#F00',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
---
+++ Home.js
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from "react"
+
+export default function Home() {
+	return(
+		<View>
+			<Text> Hello! </Text>
+		</View>
+	);
+}
+
+/*export default class Home extends React.Fragment {
+	render() {
+		return(
+			<View>
+				<Text> Hello! </Text>
+			</View>
+		);
+	}
+}*/
