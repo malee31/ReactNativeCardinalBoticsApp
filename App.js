9edaf10ea4e9cbb3c0f3daeaf9a66fe5851517ed
Marvin Lee <43216761+malee31@users.noreply.github.com>
2020-08-22 22:51:44 -0700

    Merge conflicts!!!!

--- App.js
+++ App.js
@@ -1,35 +1,14 @@
-
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import 'react-native-gesture-handler';
-import * as React from 'react';
+import { StyleSheet, Text, View, TouchableHighlight, Navigator } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-<<<<<<< HEAD
-import { StyleSheet, Text, View, TouchableHighlight, Navigator } from 'react-native';
-=======
-import { StyleSheet, Text, View } from 'react-native';
 import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import React from 'react';
+import * as React from 'react';
+
 import Home from "./Home";
 import NotHome from "./NotHome";
->>>>>>> 2605ac3b232ac8772fc22c5425fef0ff826b337c
 
 const Stack = createStackNavigator();
 
-<<<<<<< HEAD
-const Drawer = createDrawerNavigator();
-
-export default function MyDrawer() {
-  return (
-    <Drawer.Navigator>
-      <Drawer.Screen name="Feed" component={Feed} />
-      <Drawer.Screen name="Article" component={Article} />
-    </Drawer.Navigator>
-  );
-}
-=======
 export default function App() {
 	return (
 		<NavigationContainer style={styles.container}>
@@ -49,8 +28,6 @@ export default function App() {
 	);
 }
 
-
-
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
@@ -59,4 +36,3 @@ const styles = StyleSheet.create({
 		justifyContent: 'center',
 	},
 });
->>>>>>> 2605ac3b232ac8772fc22c5425fef0ff826b337c
--- Home.js
+++ Home.js
@@ -1,10 +1,17 @@
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
 import React from "react"
+
+const Drawer = createDrawerNavigator();
 
 export default function Home() {
 	return(
 		<View>
 			<Text> Hello! </Text>
+			<Drawer.Navigator>
+				<Drawer.Screen name="Feed" component={Feed} />
+				<Drawer.Screen name="Article" component={Article} />
+			</Drawer.Navigator>
 		</View>
 	);
 }
