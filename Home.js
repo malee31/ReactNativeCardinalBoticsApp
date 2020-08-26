58f21bf1c8eda80312c74fdcd89dc302290af52f
Marvin Lee <43216761+malee31@users.noreply.github.com>
2020-08-25 17:13:07 -0700

    Added toggly button

--- Home.js
+++ Home.js
@@ -1,13 +1,30 @@
-import {Image, StyleSheet, Text, View, Dimensions} from 'react-native';
+import {Image, StyleSheet, Text, View, Dimensions, Button} from 'react-native';
 import React from "react"
 
 export default class Home extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			signedIn: false
+		};
+		this.signInToggle = this.signInToggle.bind(this);
+	}
+
+	signInToggle() {
+		this.setState({
+			signedIn: !this.state.signedIn
+		});
+	}
+
 	render() {
 		return(
 			<View style={{height: "100%", width: "100%"}}>
 				<Image source={require("./assets/cardinalbotics_logo_white_clear.png")}
 					resizeMode="contain"
-					style={styles.image}/>
+					style={styles.largeLogoImage}/>
+				<Button onPress={this.signInToggle}
+					title={this.state.signedIn ? "Sign Out" : "Sign In"}
+					style={styles.signInButton}/>
 			</View>
 		);
 	}
@@ -17,10 +34,15 @@ const styles = StyleSheet.create({
 	screen: {
 		width: "100%",
 		height: "100%",
-		backgroundColor: "#eaeaea",
+		backgroundColor: "#eaeaea"
 	},
-	image: {
+	largeLogoImage: {
 		width: "100%",
 		maxHeight: "25%",
+		marginVertical: 30
+	},
+	signInButton: {
+		width: "80%",
+		marginHorizontal: "10"
 	}
 });
