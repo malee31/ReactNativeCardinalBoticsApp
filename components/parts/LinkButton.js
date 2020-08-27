e01c21534ef41c4107b0d02c0f0986d868112885
Marvin Lee <43216761+malee31@users.noreply.github.com>
2020-08-26 20:21:52 -0700

    Added icons from mdi

--- components/parts/LinkButton.js
+++ components/parts/LinkButton.js
@@ -1,20 +1,60 @@
 import * as WebBrowser from 'expo-web-browser';
 import React from "react"
 import config from "../../config.json";
-import { Button } from 'react-native-paper';
+import {Button} from 'react-native-paper';
+import {Text} from "react-native";
 
 export default class LinkButton extends React.Component {
     constructor(props) {
         super(props);
-        this.redirector = this.redirector.bind(this);
+        this.openURL = this.openURL.bind(this);
+
+        let icon;
+        //Images from https://materialdesignicons.com/
+        switch (props.icon.trim()) {
+            case "googledrive":
+                icon = "google-drive";
+            break;
+            case "roster":
+            	icon = "clipboard-account";
+            break;
+            case "calendar":
+                icon = "calendar";
+            break;
+            case "forms":
+            	icon = "format-list-bulleted-square";
+            break;
+            case "website":
+            	icon = "web";
+            break;
+            case "photo":
+            	icon = "google-photos";
+            break;
+            case "youtube":
+            	icon = "youtube-tv";
+            break;
+            case "training":
+            	icon = "teach";
+            break;
+            case "grant":
+            case "money":
+            	icon = "currency-usd";
+            break;
+            case "irc":
+            case "handbook":
+                icon = "book";
+            default:
+                icon = "cogs";
+        }
+
         this.state = {
             url: props.url,
-            title: props.title,
-            icon: props.icon,
-        }
+            title: props.title || "Resource",
+            icon: icon
+        };
     }
 
-    redirector = async () => {
+    openURL = async () => {
         await WebBrowser.openBrowserAsync(this.state.url);
     }
 
@@ -24,8 +64,9 @@ export default class LinkButton extends React.Component {
                 icon={this.state.icon}
                 mode="contained"
                 color={config.colors.secondary}
-                onPress={this.redirector}>
-                {this.state.title}
+                onPress={this.openURL}
+                key={this.state.title + ": " + this.state.url + "<" + this.state.icon + ">"}>
+                <Text style={{textAlign: "left"}}>{this.state.title}</Text>
             </Button>
         );
     }
