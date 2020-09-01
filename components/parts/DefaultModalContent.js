1b5f42f1319711fcd83be1fa38beeb36d28f7255
Marvin Lee <43216761+malee31@users.noreply.github.com>
2020-08-31 23:10:10 -0700

    Changed the compiled TS to a class

--- components/parts/DefaultModalContent.js
+++ components/parts/DefaultModalContent.js
@@ -1,21 +1,44 @@
-import React from 'react';
 import {Button, StyleSheet, Text, View} from 'react-native';
+import Modal from 'react-native-modal';
+import React from 'react';
 
-const DefaultModalContent = props => (React.createElement(View, {style: styles.content},
-    React.createElement(Text, {style: styles.contentTitle}, "Please write what you did today \uD83D\uDC4B!"),
-    React.createElement(Button, {testID: 'close-button', onPress: props.onPress, title: "Sign Out"})));
+class DefaultModalContent extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			onPress: props.onPress,
+			show: props.show
+		};
+	}
+
+	render() {
+		return (
+			<Modal isVisible={this.state.show}
+				onBackdropPress={this.state.onPress}>
+				<View style={styles.content}>
+					<Text styles={styles.contentTitle}>
+						Please write what you did today \uD83D\uDC4B!
+					</Text>
+					<Button testID="close-button" onPress={this.state.onPress} title="Sign Out"/>
+				</View>
+			</Modal>
+		);
+	}
+}
+
 const styles = StyleSheet.create({
-    content: {
-        backgroundColor: 'white',
-        padding: 22,
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 4,
-        borderColor: 'rgba(0, 0, 0, 0.1)',
-    },
-    contentTitle: {
-        fontSize: 20,
-        marginBottom: 12,
-    },
+	content: {
+		backgroundColor: 'white',
+		padding: 22,
+		justifyContent: 'center',
+		alignItems: 'center',
+		borderRadius: 4,
+		borderColor: 'rgba(0, 0, 0, 0.1)',
+	},
+	contentTitle: {
+		fontSize: 20,
+		marginBottom: 12,
+	},
 });
+
 export default DefaultModalContent;
