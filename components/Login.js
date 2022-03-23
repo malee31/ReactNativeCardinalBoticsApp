fe9fd5a730ae6cf193588ac341a7fbc2091a47ad
Marvin Lee <43216761+malee31@users.noreply.github.com>
2022-03-22 22:58:41 -0700

    Convert Login component to functional

--- components/Login.js
+++ components/Login.js
@@ -1,53 +1,45 @@
 import {Image, Text, TouchableHighlight, View} from 'react-native';
 import {TextInput} from 'react-native-paper';
-import React from "react";
+import React, { useState } from "react";
 import config from "../config.json";
 import Styles from "./parts/Styles";
 import {bindActionCreators} from "redux";
 import {setErrorMessage} from "./parts/reducerActions";
 import {connect} from "react-redux";
+import logo from "../assets/cardinalbotics_logo_white_clear.png";
 
-class Login extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			ID: ""
-		};
-		this.login = this.login.bind(this);
-	}
-
-	login() {
-		this.props.setPassword(this.state.ID, () => {
-			this.props.setErrorMessage("Verifying that you exist.");
+function Login(props) {
+	const [Id, setId] = useState("");
+	const login = () => {
+		props.setPassword(Id, () => {
+			props.setErrorMessage("Verifying that you exist.");
 		}).then(successText => {
-			this.props.setErrorMessage(successText);
+			props.setErrorMessage(successText);
 		}).catch(failText => {
-			this.props.setErrorMessage(failText);
+			props.setErrorMessage(failText);
 		});
 	}
 
-	render() {
-		return (
-			<View style={Styles.screen}>
-				<Image source={require("../assets/cardinalbotics_logo_white_clear.png")}
-					resizeMode="contain"
-					style={Styles.largeLogoImage}/>
-				<TextInput
-					label="Login"
-					value={this.state.ID}
-					style={Styles.whatchuDoing}
-					onChange={newText => this.setState({ID: newText.nativeEvent.text})}/>
-				<TouchableHighlight onPress={this.login}
-					activeOpacity={0.7}
-					underlayColor={config.colors.darkGray}
-					style={Styles.signInButton}>
-					<View>
-						<Text>Submit</Text>
-					</View>
-				</TouchableHighlight>
-			</View>
-		);
-	};
+	return (
+		<View style={Styles.screen}>
+			<Image source={logo}
+				resizeMode="contain"
+				style={Styles.largeLogoImage}/>
+			<TextInput
+				label="Login"
+				value={Id}
+				style={Styles.whatchuDoing}
+				onChange={newText => setId(newText.nativeEvent.text)}/>
+			<TouchableHighlight onPress={login}
+				activeOpacity={0.7}
+				underlayColor={config.colors.darkGray}
+				style={Styles.signInButton}>
+				<View>
+					<Text>Submit</Text>
+				</View>
+			</TouchableHighlight>
+		</View>
+	);
 }
 
 const mapDispatchToProps = dispatch => (
