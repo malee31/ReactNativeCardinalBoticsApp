763d76ef0a4f03accd003d2105886370ca53f39a
Marvin Lee <43216761+malee31@users.noreply.github.com>
2022-03-26 13:59:15 -0700

    Leaderboard now fully functioning and refreshing userInfo

--- components/pages/Leaderboard.js
+++ components/pages/Leaderboard.js
@@ -4,6 +4,7 @@ import config from "../../config.json";
 import React, { useEffect, useState } from "react";
 import MenuButton from "../parts/MenuButton";
 import { getLeaderboard } from "../parts/serverClient";
+import useUserInfo from "../parts/UserInfoProvider";
 
 function formatTime(totalTime) {
 	// let currentTime = Math.round((new Date()).getTime() / 1000) - entry.lastTime;
@@ -14,15 +15,43 @@ function formatTime(totalTime) {
 
 export default function Leaderboard({navigation}) {
 	const [leaderboardData, setLeaderboardData] = useState([]);
+	const userInfo = useUserInfo(false);
 	let content = <ActivityIndicator size="large" color={config.colors.primary}/>;
 
-	useEffect(() => {
+	const update = () => {
 		getLeaderboard()
 			.then(val => {
-				console.log(val);
 				setLeaderboardData(val);
+				if(userInfo.data.password) {
+					const user = val.find(entry => entry.name.trim() === userInfo.data.name.trim());
+					const clockedIn = Date.now() - user.timeIn;
+					if(Boolean(user.signedIn) !== Boolean(userInfo.data.signedIn)) {
+						console.log("RESYNC");
+						if(user.signedIn) {
+							userInfo.updateData({
+								signedIn: clockedIn
+							});
+						} else {
+							userInfo.updateData({
+								signedIn: 0
+							});
+						}
+					} else if(user.signedIn && Math.abs(userInfo.data.signedIn - clockedIn) > 2000 /* 2 second desync tolerance */) {
+						console.log(`Resync gap: ${Math.abs(userInfo.data.signedIn - clockedIn)}`);
+						userInfo.updateData({
+							signedIn: clockedIn
+						});
+					}
+				}
 			});
-	}, []);
+	};
+
+	useEffect(() => {
+		update();
+		// TODO: Sync more accurately using websockets
+		const timer = setInterval(update, 30 * 1000);
+		return () => clearInterval(timer);
+	}, [userInfo.data]);
 
 	if(leaderboardData) {
 		content = <FlatList
@@ -36,7 +65,7 @@ export default function Leaderboard({navigation}) {
 						<Text style={{
 							color: entry.signedIn ? "green" : "black",
 							fontSize: 16,
-						}}>{entry.name}: {formatTime(entry.totalTime)}{Boolean(entry.signedIn) && ` + ${formatTime(entry.timeIn / 1000)}`}</Text>
+						}}>{entry.name}: {formatTime(entry.totalTime / 1000)}{Boolean(entry.signedIn) && ` + ${formatTime(entry.timeIn / 1000)}`}</Text>
 					</View>
 				);
 			}}
