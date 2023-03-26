2c933fe5881b9544de1fb74e260ba70912bad72e
Marvin Lee <marvinlee.github@gmail.com>
2023-03-25 23:44:07 -0700

    Partial lenient date range parsing added for admin page

--- components/pages/Admin.js
+++ components/pages/Admin.js
@@ -160,6 +160,7 @@ export default function Admin() {
 					style={adminStyles.adminInput}
 					onChange={newText => updateAmend("password", newText.nativeEvent.text)}
 				/>
+				<DateInput/>
 				<Text>Validation is a WIP. Try to keep the format consistent</Text>
 				<TextInput
 					mode="outlined"
@@ -190,7 +191,6 @@ export default function Admin() {
 				>
 					Exit Admin Controls
 				</Button>
-
 			</View>
 		</Screen>
 	);
@@ -237,6 +237,175 @@ function AdminPasswordNeeded({ setAdminPassword }) {
 	);
 }
 
-function DateInput() {
+function DateInput({ setStart, setEnd }) {
+	const { current: dateRef } = useRef(new Date());
+	dateRef.setSeconds(0);
+	dateRef.setMilliseconds(0);
 
+
+	const [timeRangeInput, setTimeRangeInput] = useState("");
+	// Split by commas, periods, and spaces. With extra spaces removed
+	const dateParts = timeRangeInput.split(/\s+|\s*\.\s*|\s*,\s*/g).map(val => {
+		return {
+			raw: val,
+			lower: val.toLowerCase()
+		};
+	});
+
+	// Parsing
+	const delimitIndex = dateParts.findIndex(val => val.lower === "to" || val.lower === "until" || val.lower === "-");
+	const startParts = dateParts;
+	let endParts = [];
+	if(delimitIndex !== -1) {
+		endParts = startParts.splice(delimitIndex, startParts.length - delimitIndex);
+		endParts.shift();
+	}
+
+	const start = parseDatePart(startParts);
+	const end = parseDatePart(endParts);
+
+	const parsedStart = "";
+	const parsedEnd = "";
+	console.log(dateParts)
+
+	return (
+		<>
+			<Text>
+				{JSON.stringify(start)} to {JSON.stringify(end)}
+			</Text>
+			<TextInput
+				mode="outlined"
+				value={timeRangeInput}
+				label="Amended Time Range"
+				onChange={newText => setTimeRangeInput(newText.nativeEvent.text)}
+				style={adminStyles.adminInput}
+			/>
+		</>
+	);
+}
+
+function parseDatePart(parts) {
+	const result = {
+		year: undefined,
+		month: undefined,
+		day: undefined,
+		hour: undefined,
+		minute: undefined,
+		second: undefined,
+		milliseconds: undefined
+	};
+
+	for(let partIndex = 0; partIndex < parts.length; partIndex++) {
+		const part = parts[partIndex];
+		const nextPart = partIndex + 1 !== parts.length ? parts[partIndex + 1] : null;
+		let partStr = part.lower;
+
+		// Parse like a date: (1/1/1999, 1/1, 1999/1/1, 1/1999, and '-' equivalents)
+		// Leading years must be in 4-digit form but trailing years can be in short form
+		if(partStr.includes("-") || partStr.includes("/")) {
+			const sections = partStr.split(/[\-\/]/g);
+			// Reorder sections into Year, Month, Day order with heuristics
+
+			// Take out leading or trailing years in full 4-digit form
+			if(sections[0].length === 4 && !isNaN(Number(sections[0]))) {
+				result.year = Number(sections.shift());
+			}
+			const lastSection = sections[sections.length - 1];
+			if(lastSection.length === 4 && !isNaN(Number(lastSection))) {
+				result.year = Number(sections.pop());
+			}
+
+			// Parse remaining mm/dd/yy forms
+			if(sections.length) {
+				result.month = Math.min(12, Number(sections.shift()));
+			}
+			if(sections.length) {
+				result.day = Math.min(31, Number(sections.shift()));
+			}
+			if(sections.length) {
+				result.year = Number(sections.shift());
+			}
+			continue;
+		}
+
+		// Parse days in the form of 1st, 2nd, 3rd, etc
+		if((/^\d+(st|nd|th|rd)$/).test(partStr)) {
+			const dayStr = partStr.match(/^\d+/)[0];
+			if(Number(dayStr) < 31) {
+				result.day = Number(dayStr);
+			}
+			continue;
+		}
+
+		// Parse full years
+		if(partStr.length === 4 && !isNaN(Number(partStr))) {
+			result.year = Number(partStr);
+			continue;
+		}
+
+		// Parse month names
+		// Index-sensitive array of months
+		const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+		const monthIndex = months.findIndex(month => partStr.length >= 3 && month.toLowerCase().startsWith(partStr));
+		if(monthIndex !== -1) {
+			result.month = monthIndex + 1;
+			continue;
+		}
+
+		// Parse lone hours or days
+		// If a number is alone, it is assumed to be a day unless immediately followed by AM/PM
+		if(!isNaN(Number(partStr))) {
+			if(nextPart && (/^[ap]m$/).test(nextPart.lower)) {
+				result.hour = Number(partStr);
+				continue;
+			}
+			result.day = Number(partStr);
+			continue;
+		}
+
+		// Parse times in 8:00 format
+		if(partStr.includes(":")) {
+			const sections = partStr.split(":");
+			const lastSection = sections[sections.length - 1];
+			let pm = false;
+			if(lastSection.endsWith("am") | lastSection.endsWith("pm")) {
+				// Edit final section to remove am/pm
+				pm = lastSection.slice(-2) === "pm";
+				sections[sections.length - 1] = lastSection.substring(0, lastSection.length - 2);
+			}
+
+			// Take parts off one at a time until none are left
+			// Order is expected to be hh:mm:ss:ms and can be cut short (so hh:mm is valid)
+			// AM/PM will be taken into account
+			if(sections.length) {
+				result.hour = Math.min(23, Number(sections.shift()));
+				if(pm || (nextPart && nextPart.lower === "pm")) {
+					result.hour = (result.hour % 12) + 12;
+				}
+			}
+			if(sections.length) {
+				result.minute = Math.min(59, Number(sections.shift()));
+			}
+			if(sections.length) {
+				result.second = Math.min(59, Number(sections.shift()));
+			}
+			if(sections.length) {
+				result.milliseconds = Math.min(999, Number(sections.shift()));
+			}
+			continue;
+		}
+
+		// Just an hour and a am/pm
+		if((/^\d+[ap]m$/g).test(partStr)) {
+			result.hour = Number(partStr.match(/^\d+/));
+			if(partStr.endsWith("pm")) {
+				result.hour = (result.hour % 12) + 12;
+			}
+			continue;
+		}
+
+		console.log(`Unknown part in date: ${part.raw}`);
+	}
+
+	return result;
 }
